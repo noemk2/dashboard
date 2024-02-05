0e0579c38b701a064bf2469491bd353469bd9d3b
Noe <noemk2@gmail.com>
2024-02-05 15:24:54 +0000

    conda

---
+++ action.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Descargar el instalador de Miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+
+# Dar permisos de ejecución al instalador
+chmod +x miniconda.sh
+
+# Instalar Miniconda en un directorio específico (puedes cambiar '/opt/miniconda' a tu preferencia)
+./miniconda.sh -b -p /home/gitpod/miniconda3
+
+# Agregar el directorio bin de Miniconda al PATH (esto se añadirá automáticamente al final del archivo .bashrc)
+# echo 'export PATH="/home/gitpod/miniconda3/bin:$PATH"' >> ~/.profile
+
+# Recargar el archivo de perfil para que los cambios en el PATH tengan efecto inmediato
+source ~/.profile
+
+# Borrar el instalador
+rm miniconda.sh
+
+echo "Miniconda se ha instalado correctamente."
